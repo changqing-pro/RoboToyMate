bb96e119795cc646e2091519bba9aab372f011ec
songzonglin <1874091239@qq.com>
2024-09-11 17:00:53 +0800

    init

---
+++ src/main/java/com/robo/RoboToyMateApplication.java
@@ -0,0 +1,13 @@
+package com.robo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboToyMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoboToyMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robo/RoboToyMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboToyMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
